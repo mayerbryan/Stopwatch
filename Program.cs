47c703a899e9f7d55dfdfd25b7ad72d8e4155534
Bryan Mayer <mayerbryan@mail.com>
2022-03-17 11:50:08 -0300

    final

--- Program.cs
+++ Program.cs
@@ -1,41 +1,85 @@
 ﻿using System;
 
-namespace Stopwatch{
-    class Program{
+namespace Stopwatch
+{
+    class Program
+    {
         static void Main(string[] args)
         {
             Menu();
         }
 
-        static void Menu(){
+        static void Menu()
+        {
             Console.Clear();
-            Console.WriteLine("digite o tempo desejado em horas");
-            int horas = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("digite o tempo desejado em minutos");
-            int minutos = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("digite o tempo desejado em segudos");
-            int segundos = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("pressione 1 para começar ou 2 para resetar");
-            int selecao = Int32.Parse(Console.ReadLine());
-            switch(selecao){
-                case 1: Start(horas, minutos, segundos); break;
-                case 2: Console.Clear(); Menu(); break;
+            Console.WriteLine("0 to exite the application");
+            Console.WriteLine("1 for Stopwatch");
+            Console.WriteLine("2 for CountDown Timer");
+            short option = short.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: StopWatchMenu(); break;
+                case 2: CountDownMenu(); break;
             }
-           
         }
 
-        static async void Start(int hour, int min, int sec){
-            min = min*60;
-            int currentTime = min+sec;
-            while(currentTime > -1){
+        static void StopWatchMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Press Enter to start");
+            Console.ReadLine();
+            int time = 0;
+            while (!Console.KeyAvailable)
+            {
+                Console.Clear();
+                time++;
+                Console.WriteLine(time);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Final Time: "+ time);
+            Console.WriteLine("Press Enter to go back to Menu");
+            Console.ReadLine();
+            Console.ReadLine();
+            Menu();
+            
+        }
+
+        static void CountDownMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Type the amount of hours");
+            int horas = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Type the amount of minutes");
+            int minutos = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Type the amount of seconds");
+            int segundos = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Press 1 to start or 2 to reset");
+            short selecao = short.Parse(Console.ReadLine());
+            switch (selecao)
+            {
+                case 1: CountDownStart(horas, minutos, segundos); break;
+                case 2: Console.Clear(); CountDownMenu(); break;
+            }
+        }
+
+        static async void CountDownStart(int hour, int min, int sec)
+        {
+            min = min * 60;
+            int currentTime = min + sec;
+            while (currentTime > -1)
+            {
                 Console.Clear();
                 TimeSpan t = TimeSpan.FromSeconds(currentTime);
                 Console.WriteLine(t.ToString());
-                currentTime--;                
+                currentTime--;
                 Thread.Sleep(1000);
             }
-            Console.WriteLine("cronometro finalizado!");
+            Console.WriteLine("Count Down finished!");
             Thread.Sleep(2500);
+            Console.WriteLine("Press Enter to go back to Menu");
+            Console.ReadLine();
             Menu();
         }
     }
